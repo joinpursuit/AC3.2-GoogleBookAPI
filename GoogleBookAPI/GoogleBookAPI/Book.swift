dc44f99215300f81cd76b3874f1b434f7b82435d
defintuition <ahspadafora@gmail.com>
2016-12-05 02:29:04 -0500

    hw

---
+++ GoogleBookAPI/GoogleBookAPI/Book.swift
@@ -0,0 +1,28 @@
+//
+//  Book.swift
+//  GoogleBookAPI
+//
+//  Created by Amber Spadafora on 12/3/16.
+//  Copyright © 2016 C4Q. All rights reserved.
+//
+
+import Foundation
+
+class Book {
+    
+    let bookID: String
+    let title: String
+    let authors: [String]
+    let smallThumbnail: String?
+
+    init(bookID: String, title: String, smallThumbnail: String?,authors: [String]){
+        self.bookID = bookID
+        self.title = title
+        self.authors = authors
+        self.smallThumbnail = smallThumbnail
+    }
+    
+    
+    
+    
+}
